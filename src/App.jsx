d2fbc91f86911d9444202e5799f781fc12547286
Jamesupback <jamessajan07@gmail.com>
2025-04-01 00:05:00 +0530

    all done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from '../pages/Home';
 import Api from '../pages/Api';
 import Chat from '../pages/Chat';
